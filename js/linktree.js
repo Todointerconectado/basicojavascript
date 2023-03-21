8a7793179c1e8172fcec40cdfcae2159bb024fc6
Francisco Carusso <francisconicolascarusso@gmail.com>
2023-03-21 06:14:34 -0300

    Selector de genero

---
+++ js/linktree.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('The DOM document has been fully loaded.');
+});
+
+
+let gender = document.getElementById('gender');
+let man = document.getElementById('man');
+let women = document.getElementById('women');
+let profileImg = document.getElementById('profileImg');
+
+function selectGender(event) {
+
+    let etiqueta = event.target.tagName;
+    let id  = event.target.value;
+    // let src = profileImg.getAttribute('src');
+
+    if(event.target && etiqueta === 'BUTTON') {
+
+        if(id === 'man' && women.classList.value === 'btnGender btnGenderSwitchedOn'){
+            women.classList.remove('btnGenderSwitchedOn');
+            profileImg.setAttribute('src', '../images/linktree/avatar-ej1.png');
+        }
+        if(id === 'women' && man.classList.value === 'btnGender btnGenderSwitchedOn'){
+            man.classList.remove('btnGenderSwitchedOn');
+            profileImg.setAttribute('src', '../images/linktree/avatar-ej2.png');
+        }
+
+        event.target.classList.toggle('btnGenderSwitchedOn');
+    }
+}
+
+gender.addEventListener('click', selectGender);
