d6c132e7650c340c6d5b7663b337fbdb934592f0
Juan Paz <jpaz.dev94@gmail.com>
2023-11-11 02:58:58 -0300

    Callbacks.js - Finalizando concepto de Callbacks

--- script/Callbacks.js
+++ script/Callbacks.js
@@ -2,7 +2,7 @@
 
 // ---------------------------- Callback ----------------------------
 // Las callback son funciones que se pasan como argumento a otra funcion
-// Esta tecnica nos permite tener una mejor secuencia de control sobre la ejecucion de nuestras funciones y tener codigo reutilizable.
+// Esta tecnica nos permite tener una mejor secuencia de control sobre la ejecucion de nuestras funciones y tener codigo reutilizable y flexibilidad.
 
 //Por ejemplo tenemos cuatro funciones llamadas suma,resta,multiplicacion,division y que nos devuelve el resultado de dicha operacion
 //Y una funcion llamada calculadora, que muestra el resultado
@@ -33,7 +33,8 @@ calculadora(resultadoResta); // Output -> 11
 calculadora(resultadoMutiplicacion); // Output -> 19
 calculadora(resultadoDivision); // Output -> 94
 
-//Este ejemplo tiene un problema , y es que tenemos que crear una referencia a cada operacion y luego llamar a la calculadora para que muestre cada uno de estos resultados
+//Este ejemplo tiene un problema:
+//Es que tenemos que crear una referencia a cada operacion y luego llamar a la calculadora para que muestre cada uno de estos resultados
 
 // Con el uso de las callback podemos ahorrarnos una parte de ese codigo. Podemos modificar nuestra funcion calculadora,
 // y que en ves de recibir 1 parametro para mostrar el resultado, reciba 3 parametros: Los operando (a y b) y el operador:
@@ -63,7 +64,9 @@ calculadora(resultadoDivision); // Output -> 94
 }
 
 //Las callbacks tambien nos permiten definir una nueva funcion que no haya sido definido previamente
-//utilizando una funcion anonima o funcion de flecha, que nos permiten crear nuevas funciones sobre la marcha, sin la necesidad de definirlas previamente
+//utilizando una funcion anonima o funcion de flecha,
+//que nos permiten crear nuevas operaciones sobre la marcha,
+//otorgandonos mayor flexibilidad en nuestro codigo.
 
 {
   function calculadora(a, b, operador) {
@@ -72,13 +75,48 @@ calculadora(resultadoDivision); // Output -> 94
   }
 
   //Con una funcion anonima:
-  calculadora(25,25,function(a,b){
-    return (a+b)*10
-  }) //output -> 500
+  calculadora(25, 25, function (a, b) {
+    return (a + b) * 10;
+  }); //output -> 500
+
   //Con una funcion de flecha:
-  calculadora(500,250,(a,b)=>{
-    return (a-b)/10
-  })//output -> 25
+  calculadora(500, 250, (a, b) => {
+    return (a - b) / 10;
+  }); //output -> 25
 }
 
-//En estos ejemplos creamos dos nuevas operaciones
+//Ademas de otorganos codigo reutilizable y flexible,
+//las callbacks requieren cierta consitencia en los parametros que esperan al ser invocadas.
+//Cuando nuestra calculadora invoca al operador, espera que reciba dos parametros.
+//La callback operador, puede o no utilizar esos parametros,
+//pero la calculadora REQUIERE que sigua una estructura predefinida.
+//De lo contrario, podrian surgir errores inesperados
+
+{
+  function calculadora(a, b, operador) {
+    let resultado = operador(a, b);
+    console.log(resultado);
+  }
+
+  //Recibe los parametros a y b, pero no las utiliza.
+  calculadora(300, 600, function (a, b) {
+    return 2;
+  }); //output -> 2
+
+  //Recibe los parametros a y b, los modifica y retorna otro valor esperado
+  calculadora(25, 11, function (a, b) {
+    a = "25";
+    b = "11";
+    return `${a} + ${b}`;
+  }); //output -> '25 + 11'
+
+  //No recibe ningun parametro, realiza su propia operacion y provoca un resultado inesperado la calculadora.
+  calculadora(600, 400, function () {
+    return 100 + 100;
+  }); //output -> 200
+
+  //Recibe los parametros a y b, y se suma un parametro c, provocando un resultado inesperado para la calculadora.
+  calculadora(600, 400, function (a, b, c) {
+    return a + b + c;
+  }); //output -> NaN
+}
